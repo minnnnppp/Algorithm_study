757617d343d24941e731ba700b2c8df6753e90d3
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2023-04-25 23:17:51 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,19 +1,15 @@
 def solution(progresses, speeds):
     answer = []
-    time = 0
-    count = 0
-    
-    while len(progresses)> 0:
-        if (progresses[0] + time*speeds[0]) >= 100: # 100프로 완료되었다면
-            progresses.pop(0)   # 추출해서
-            speeds.pop(0)       
-            count += 1          # 개수 카운팅하기
-            
+    days, cnt = 0, 0
+    while len(progresses) > 0:
+        if(progresses[0]+days*speeds[0])>=100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
         else:
-            if count > 0:       # 한 개 이상 완료되었다면
-                answer.append(count)    # answer에 카운트를 반영해서 추가하고
-                count = 0       # 카운트를 다시 0으로 만들기
-            time += 1           # 아무것도 완료되지 않았다면 해당 작업의 작업 날짜만 하루 더하기
-            
-    answer.append(count)        # while문 반복 종료되면 마지막 카운트를 answer에 추가하기
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            else: days+=1
+    answer.append(cnt)
     return answer
