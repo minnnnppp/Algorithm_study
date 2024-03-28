e5abdcbd0ae587fcfa9966b14bf2a658ebe8a714
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-03-28 17:15:39 +0900

    [level 2] Title: 올바른 괄호, Time: 8.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,19 +1,18 @@
 def solution(s):
-    cnt = 0
-    if s[0]=='(':
-        for _s in s:
-            if _s == '(':
-                cnt +=1
+    if s[0] == ')' or s[-1] == '(':
+        return False
+    
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) >0:
+                stack.pop()
             else:
-                cnt -=1
-                if cnt < 0:
-                    return False
-                
-        if cnt == 0 and s[-1]==')':
-            return True
-        else: 
-            return False
-                
+                continue
+    
+    if len(stack) ==0:
+        return True
     else:
         return False
-        
