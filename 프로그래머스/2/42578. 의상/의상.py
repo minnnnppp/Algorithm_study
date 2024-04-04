85f036174699ce80d5656eb31ff76b8ab441f9e6
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-04-04 14:56:39 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,11 @@
-from collections import Counter
 from functools import reduce
 def solution(clothes):
-    # 1) 종류별 가짓수 집계
-    _dict = Counter([kind for name, kind in clothes])
-    # 2) 총 조합수 집계
-    answ = reduce(lambda x, y: x*(y+1), _dict.values(), 1) - 1
+    answ = 1
+    dict_cnt = {x[1]:0 for x in clothes}
+    for i in clothes:
+        dict_cnt[i[1]]+=1
     
-    return answ
+    for v in dict_cnt.values():
+        answ*=(v+1)
     
+    return answ-1
