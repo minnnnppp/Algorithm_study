2ad229548a4eabe95cfe509f5ae9aaea42e023cc
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-04-09 14:06:27 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,22 +1,17 @@
 def solution(n, times):
-    _l = min(times)
-    _r = max(times)*n
-    
-    while _l <= _r:
-        _m = (_r+_l)//2
-        cnt = 0
+    times.sort()
+    mint, maxt = 1, max(times)*n
+    while mint <= maxt:
+        midt = (maxt+mint)//2
+        past = 0
         for t in times:
-            cnt += _m//t
-            if cnt >= n: break
+            past += midt//t
+            if past == n:
+                break
+        if past >= n:
+            sec = midt
+            maxt = midt-1
+        elif past < n:
+            mint = midt+1
             
-        if cnt >= n: 
-            answ = _m
-            _r = _m-1
-        elif cnt < n:
-            _l = _m+1
-            
-    return answ
-        
-            
-            
-            
+    return sec
