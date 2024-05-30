af876aa42fcb324efd072d7331817b1042c15d92
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-05-30 17:00:52 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,27 +1,17 @@
 def solution(n, lost, reserve):
-    # 1. make list of total student
-    lost.sort()
-    reserve.sort()
-    target = [i for i in range(1, n+1)]
-    # 2. find intersection between lost and reserve
-    intersec = [x for x in lost if x in reserve]
-    if intersec:
-        lost = [x for x in lost if x not in intersec]
-        reserve = [x for x in reserve if x not in intersec]
+    # 1. 체육복 갖고 있는 학생 파악
+    reserve_cp = [i for i in reserve if i not in lost]
+    lost_cp = [x for x in lost if x not in reserve]
+    reserve_cp.sort()
     
-    # 3. consider if student can borrow extra uniform
-    for i in range(1, n+1):
-        if i in lost:
-            target_bool = i in reserve
-            if target_bool == True:
-                lost.remove(i)
-                reserve.remove(i)
-            else:
-                can = [x for x in [i-1, i+1] if x in reserve]
-                if len(can) > 0:
-                    reserve.remove(min(can))
-                    lost.remove(i)
+    # 2. 최종 분실 학생 집계해 체육복 보유 학생 수 계산
+    for r in reserve_cp:
+        b = r-1
+        a = r+1
+        if b in lost_cp:
+            lost_cp.remove(b)
+        elif a in lost_cp:
+            lost_cp.remove(a)
     
-    return len([y for y in target if y not in lost])
-    
+    return n-len(lost_cp)
     
