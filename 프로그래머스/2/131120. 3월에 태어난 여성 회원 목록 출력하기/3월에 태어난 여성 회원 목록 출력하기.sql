b9824902ebe22d9864ce257fa09734de93ea86e9
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-07-22 15:35:36 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,5 +1,5 @@
--- 코드를 입력하세요
-select MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+select MEMBER_ID,MEMBER_NAME,GENDER
+    , date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
 from MEMBER_PROFILE
-where (month(DATE_OF_BIRTH)='3') and (TLNO is not null) and (GENDER = 'W')
+where month(DATE_OF_BIRTH) = 3 and GENDER = 'W' and TLNO is not null
 order by MEMBER_ID
