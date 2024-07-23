7c4452db376801e5f601068bb0146d2bdafe6191
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-07-23 12:49:39 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT USER_ID, PRODUCT_ID
+select USER_ID, PRODUCT_ID
 from ONLINE_SALE
 group by USER_ID, PRODUCT_ID
-having count(ONLINE_SALE_ID) > 1
-order by USER_ID asc, PRODUCT_ID desc;
+having count(*) > 1
+order by USER_ID asc, PRODUCT_ID desc
