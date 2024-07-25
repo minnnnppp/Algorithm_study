bce4718ad0d052c9f4e431f17a06d62990195cf1
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-07-26 00:07:34 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,8 @@
+from collections import Counter
 from functools import reduce
 def solution(clothes):
-    answ = 1
-    dict_cnt = {x[1]:0 for x in clothes}
-    for i in clothes:
-        dict_cnt[i[1]]+=1
+    dict_cnt = Counter([x[1] for x in clothes])
+    answ = reduce(lambda x, y: x*(y+1), dict_cnt.values(), 1) - 1
     
-    for v in dict_cnt.values():
-        answ*=(v+1)
-    
-    return answ-1
+    return answ
+        
