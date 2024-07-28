16de7261ec42ca617787fe8738db59140bdf3824
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-07-28 15:34:48 +0900

    [level 3] Title: 베스트앨범, Time: 0.93 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,29 @@
-import operator
 def solution(genres, plays):
+    _genres_total = {}
+    _plays = sorted(list(enumerate(plays)), key = lambda x: x[1], reverse = True)
+    _genres = list(enumerate(genres))
+    _plays_idx = {}
     answ = []
-    plays = list(enumerate(plays))
-    _dict = {}
-    _dict_sum = {}
-    for g, p in zip(genres, plays):
-        if g in _dict.keys():
-            _dict[g].append(p)
-            _dict_sum[g] += p[1]
-        else:
-            _dict[g] = [p]
-            _dict_sum[g] = p[1]
-    lst_rev_g = [
-        x[0] for x in sorted(
-            _dict_sum.items(), key=operator.itemgetter(1), reverse=True
-        )]
     
-    for g in lst_rev_g:
-        _target = sorted(_dict[g], key = lambda x: x[1], reverse=True)
-        answ.extend([x[0] for x in _target[:2]])
-        
+    for g, p in zip(genres, plays):
+        if g in _genres_total:
+            _genres_total[g].append(p)
+        else:
+            _genres_total[g] = [p]
+
+    # 2) 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+    for p in _plays:
+        _g = [x[1] for x in _genres if x[0]==p[0]][0]
+        if _g in _plays_idx.keys():
+            _plays_idx[_g].append(p[0])
+        else:
+            _plays_idx[_g] = [p[0]]
+
+    _plays_idx
+
+    # 3) 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+    for x in sorted(_genres_total.items(), key=lambda x : sum(x[1]), reverse=True):
+        _g = x[0]
+        answ += _plays_idx[_g][:2]
+    
     return answ
