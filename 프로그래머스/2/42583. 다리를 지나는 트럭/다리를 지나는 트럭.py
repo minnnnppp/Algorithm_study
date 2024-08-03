2658d82390e5ba78bdd49d1fb00b9f0e410b338f
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-03 22:40:25 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 90.31 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,21 +1,25 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    truckq = deque(truck_weights)
-    iscnt = deque([0 for i in range(bridge_length)])
-    sec, weightsum = 0, 0
+    total_cnt = len(truck_weights)
+    truck_weights = deque(truck_weights) 
+    now = deque([])
+    times = deque([])
+    finish = []
+    answ = 0
+
     while True:
-        if not truckq:
-            return sec + bridge_length
+        answ += 1
+        ## 다 건넌 트럭 있는지 확인
+        if now and answ - times[0] == bridge_length:
+            finish.append(now.popleft())
+            times.popleft()
+            
+            ### 모든 트럭이 통과하면 정답 반환
+            if len(finish) == total_cnt:
+                return answ
+
+        ## 다리를 건널 트럭 있는지 확인
+        if truck_weights and (sum(now)+truck_weights[0]<=weight):
+            now.append(truck_weights.popleft())
+            times.append(answ)
         
-        _minus = iscnt[0]
-        _add = truckq[0]
-        if weightsum - _minus + _add <= weight:
-            weightsum = weightsum - _minus + _add
-            iscnt.popleft()
-            target = truckq.popleft()
-            iscnt.append(target)
-        else:
-            weightsum -= _minus
-            iscnt.popleft()
-            iscnt.append(0)
-        sec+=1
