308d1d129a321e60c3a903a804af1eb5d41b8707
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-07 15:05:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 266.27 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,29 +1,31 @@
 import heapq
 def solution(operations):
-    heap = []          
-    heap_max = []
+    min_heap = []
+    max_heap = []
     
-    for o in operations:
-        if o[0] == 'I':
-            target = int(o.split(' ')[1])
-            neg_target= (-1)*target
-            heapq.heappush(heap, target)
-            heapq.heappush(heap_max, (neg_target, target))
-        elif o == 'D -1' and len(heap) >0 and len(heap_max)>0:
-            target = heapq.heappop(heap)
-            heap_max.remove(((-1)*target, target))
-        elif o == 'D 1' and len(heap) >0 and len(heap_max)>0:
-            target = heapq.heappop(heap_max)
-            heap.remove(target[1])
-            
-    # res = [x for x in heap if (-x, x) in heap_max]
-                               
-    
-    if len(heap) == 0:
+    for x in operations:
+        comm, num = x.split()
+        num = int(num)
+        
+        ## 힙에 값 추가(최소/최대힙에 모두 추가)
+        if comm == 'I':
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, (-1)*(num))
+        else:
+            if num == -1:
+                try: 
+                    y = heapq.heappop(min_heap)
+                    max_heap.remove((-1)*y)
+                except IndexError:
+                    pass
+            else:
+                try: 
+                    y = heapq.heappop(max_heap)
+                    min_heap.remove((-1)*y)
+                except IndexError:
+                    pass
+                
+    try : 
+        return [max(min_heap), min(min_heap)]
+    except:
         return [0, 0]
-    else:
-        return [max(heap), min(heap)]
-    
-    
-    
-    
