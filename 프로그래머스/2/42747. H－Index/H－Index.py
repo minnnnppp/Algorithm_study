1c862e7f2ac7a094fe13960deab007bcf43ebc4d
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-09 18:02:27 +0900

    [level 2] Title: H-Index, Time: 263.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,5 @@
 def solution(citations):
-    citations = sorted(citations, reverse = True)
-    if max(citations) == 0:
-        return 0
-    else:
-        for i in range(len(citations)):
-            if citations[i] <= i:
-                return i
-        return len(citations)
+    for i in range(max(citations),-1,-1):
+        a=[1 for j in citations if j>=i]
+        if len(a) >=i :
+            return i
