b906f4efbaff86d0985ec6f2d89e164f8998d661
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-14 12:02:12 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,8 +1,6 @@
-select case when rk = 1 then ID end as ID, FISH_NAME, LENGTH
-from (    
-    select *, rank() over(partition by FISH_TYPE order by LENGTH desc) as rk
-    from FISH_INFO 
-) i
+select ID, FISH_NAME, LENGTH
+from FISH_INFO i
     join FISH_NAME_INFO n on i.FISH_TYPE = n.FISH_TYPE
-having ID is not null
+where (i.FISH_TYPE, LENGTH) in (
+    select FISH_TYPE, max(LENGTH) over(partition by FISH_TYPE) from FISH_INFO)
 order by 1;
