e2911b93a19ef8d725775d8bd1c8b5e4c242f0b2
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-15 14:33:16 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,8 +1,6 @@
-select case 
-        when PRICE >= 10000 then truncate(PRICE, -4) 
-        else 0
-    end as PRICE_GROUP
+select case when PRICE < 10000 then 0 else truncate(PRICE, -4)
+        end as PRICE_GROUP
     , count(distinct PRODUCT_ID) as PRODUCTS
-from PRODUCT 
+from PRODUCT
 group by PRICE_GROUP
-order by PRICE_GROUP
+order by 1;
