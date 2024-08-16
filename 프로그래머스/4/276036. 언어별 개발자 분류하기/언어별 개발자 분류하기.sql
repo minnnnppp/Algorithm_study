ef352a9ff013a654656ec00871ef55b0f4925ad5
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-16 22:21:22 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,32 +1,10 @@
-# with ad as (
-#     select ID
-#     from DEVELOPERS
-#     where SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python')
-#         and SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY = 'Front End')
-# ), bd as (
-#     select ID
-#     from DEVELOPERS
-#     where SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#')
-# )
-# , cd as (
-#     select ID
-#     from DEVELOPERS
-#     where (ID not in (select ID from ad)) and (ID not in (select ID from bd))
-#         and (SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY = 'Front End'))
-# )
-
-select case when 
-            SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python')
-            and SKILL_CODE & (
-                select sum(CODE) from SKILLCODES 
-                where CATEGORY = 'Front End'
-            ) then 'A'
+select case 
+            when SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python')
+                and SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY = 'Front End') then 'A'
             when SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#') then 'B'
-            when SKILL_CODE & (
-                select sum(CODE) from SKILLCODES 
-                where CATEGORY = 'Front End'
-            ) then 'C'
-        end as GRADE, ID, EMAIL
+            when SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY = 'Front End') then 'C'
+            end as GRADE
+        , ID, EMAIL
 from DEVELOPERS
 having GRADE is not null
-order by GRADE, ID
+order by 1, 2;
