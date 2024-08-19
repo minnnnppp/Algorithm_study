d7e94927b2b6f7bd02a9c1a91defeced4e72a41e
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-20 01:52:23 +0900

    [level 2] Title: 카펫, Time: 82.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,22 +1,10 @@
 def solution(brown, yellow):
-    total_width = brown+yellow
+    total_size = brown + yellow
+    # 노란색 카펫 약수 구하기
+    y_divisors = [x for x in range(1, yellow+1) if yellow%x == 0]
     
-    def find_factors(x):
-        lst_factors = []
-        for i in range(1, x+1):
-            if x%i == 0:
-                lst_factors.append(i)
-        return lst_factors
-    
-    lst_tw_f = find_factors(total_width)
-    for x in lst_tw_f:
-        y = total_width//x
-        if (x-2)*(y-2) == yellow:
-            return [y, x]
-        
-    # mid_idx = (len(lst_tw_f)//2)
-    # if (mid_idx//2 == 0):
-    #     return [lst_tw_f[mid_idx], lst_tw_f[mid_idx]]
-    # else:
-    #     return [lst_tw_f[mid_idx], lst_tw_f[mid_idx-1]]
-    
+    # 노란색이 중앙에 오는 경우의 가로*세로 구하기
+    for x in y_divisors:
+        y = yellow/x
+        if (x+2)*(y+2) == total_size:
+            return sorted([(x+2), (y+2)], reverse =True)
