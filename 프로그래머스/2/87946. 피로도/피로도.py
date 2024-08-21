60664792477aca3e4c0c1c78fac077858e1b8ed7
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-08-21 22:48:45 +0900

    [level 2] Title: 피로도, Time: 83.53 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,17 @@
 from itertools import permutations
 def solution(k, dungeons):
-    lst_permutations = list(permutations(dungeons, len(dungeons)))
-    lst_k = []
-    for dg in lst_permutations:
-        remain, cnt = k, 0
-        for d in dg:
-            if remain >= d[0] :
-                remain-=d[1]
-                cnt+=1
-            else: 
-                break
-        lst_k.append(cnt)
-        
-    return max(lst_k)
+    lst_combs = list(permutations(dungeons, len(dungeons)))
+    answ = []
+    
+    for c in lst_combs:
+        target, cnt = k, 0
+        for d in c:
+            x, y = d[0], d[-1]
+            if target >= x:
+                cnt +=1
+                target -= y
+            else:
+                pass
+        answ.append(cnt)
+
+    return max(answ)
