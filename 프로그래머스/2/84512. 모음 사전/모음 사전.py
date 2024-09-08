0481393b86a7957b64aeff55443f58628a25a9a0
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-09-08 19:22:17 +0900

    [level 2] Title: 모음 사전, Time: 1.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,20 +1,11 @@
 from itertools import product
 def solution(word):
-    cnt = 1
-    lst_answ = list(word)
-    lst_w = ['A', 'E', 'I', 'O', 'U']
-    lst_new = []
-    
+    answ = []       
+    vowels = ['A', 'E', 'I', 'O', 'U']
+
     for i in range(1, 6):
-        for comb in product(lst_w, repeat = i):
-            lst_new.append(list(comb))
-    
-    lst_new.sort()
-    
-    for w in lst_new:
-        if w == lst_answ:
-            return cnt
-        else:
-            cnt+=1
-    
-    
+        for x in product(vowels, repeat=i):
+            answ.append(''.join(x))
+
+    answ.sort()
+    return answ.index(word)+1
