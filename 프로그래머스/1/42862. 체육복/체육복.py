1ac0b98b9a6045994e4424e2721fa3d5a798511b
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-09-15 18:12:49 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,17 +1,15 @@
 def solution(n, lost, reserve):
-    # 1. 체육복 갖고 있는 학생 파악
-    reserve_cp = [i for i in reserve if i not in lost]
-    lost_cp = [x for x in lost if x not in reserve]
-    reserve_cp.sort()
+    total = [i for i in range(1, n+1)]
+    _reserve = sorted(list(set(reserve) - set(lost)))
+    _lost = sorted(list(set(lost) - set(reserve)))
+    # _reserve_c = _reserve.copy()
+    _lost_c = _lost.copy()
     
-    # 2. 최종 분실 학생 집계해 체육복 보유 학생 수 계산
-    for r in reserve_cp:
-        b = r-1
-        a = r+1
-        if b in lost_cp:
-            lost_cp.remove(b)
-        elif a in lost_cp:
-            lost_cp.remove(a)
-    
-    return n-len(lost_cp)
-    
+    for l in _lost:
+        lst_targets = [r for r in _reserve if l == r or l == r-1 or l == r+1]
+        if lst_targets:
+            y = min(lst_targets)
+            _lost_c.remove(l)
+            _reserve.remove(y)
+            
+    return n-len(_lost_c)
