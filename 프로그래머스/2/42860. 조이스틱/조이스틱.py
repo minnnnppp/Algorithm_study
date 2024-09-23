09c70ced1ad425ffb6bc3e67b4b32dbf218b612b
Minkyung Park <93141881+minnnnppp@users.noreply.github.com>
2024-09-23 22:20:42 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,26 +1,17 @@
 def solution(name):
-    answ = 0
-    # 기본 좌우이동 횟수
-    min_move = len(name) - 1
-
-    for _i, _s in enumerate(name):
-        # 해당 문자의 변경횟수 최솟값 구하고 더해주기
-        answ += min(ord(_s) - ord('A'), ord('Z') - ord(_s) + 1)
-
-        # 해당 문자 이후 연속된 A 문자열 찾기
-        nxt = _i + 1
-        while nxt < len(name) and name[nxt] == 'A':
-            nxt += 1
-
-        # 기존, 연속된 A의 왼쪽부터 시작, 연속된 A의 오른쪽부터 시작 중 최솟값 찾음
-        # 연속된 A가 여러 곳일 수 있으니 for문 동안에 갱신
-        min_move = min([
-            min_move, 
-            2 *_i + len(name) - nxt, 
-            _i + 2 * (len(name) -nxt)
-        ])
-
-    # 문자 변경 횟수 + 위치 이동 횟수
-    answ += min_move
+    cnt = 0
+    minn = len(name)-1
     
-    return answ
+    for i, n in enumerate(name):
+        # 1) 최소 조작횟수 찾기: 정방향으로 조작 vs 반대방향으로 조작 횟수를 비교
+        cnt += min(ord(n.upper())-ord('A'), ord('Z')-ord(n.upper())+1)
+        
+        # 2) 연속된 'A' 찾기
+        next = i+1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        
+        # 3) 이동 횟수의 최솟값 구하기
+        minn = min([minn, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
+    
+    return cnt + minn
